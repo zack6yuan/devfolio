0013af20eca9b78042b505b6e77e113f157ab7cd
Zack Yuan <zack6yuan@gmail.com>
2026-07-06 23:01:19 -0500

    feat: update toolkit section

--- devfolio/app/page.tsx
+++ devfolio/app/page.tsx
@@ -6,6 +6,7 @@ import About from "./components/about";
 import Bottle from "./components/bottle";
 import StackBanner from "./components/StackBanner";
 import Work from "./components/work";
+import Toolkit from "./components/toolkit";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <StackBanner />
       <About />
       <Work />
+      <Toolkit />
       <Archipelago />
       <Backpack />
       <Bottle />
